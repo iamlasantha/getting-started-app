b015c6f28e579a5891405be77655f757024c1a19
iamlasantha <social.iamlasantha@gmail.com>
2026-02-18 09:31:39 +0530

    feat: Initial project scaffolding (unstable)

---
+++ src/routes/addItem.js
@@ -0,0 +1,13 @@
+const db = require('../persistence');
+import { v4 as uuid } from 'uuid';
+
+module.exports = async (req, res) => {
+    const item = {
+        id: uuid(),
+        name: req.body.name,
+        completed: false,
+    };
+
+    await db.storeItem(item);
+    res.send(item);
+};
